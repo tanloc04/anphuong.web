9f5573a1ccd09ae86a8f62e16ce425c7a1f13094
tanloc04 <tanloc040403@gmail.com>
2025-09-21 17:18:56 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './styles/globals.css'
 import App from './App.tsx'
 
-import 'primereact/resources/themes/lara-light-blue/theme.css'; 
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css'; 
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
