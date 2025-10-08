b2c0980a81ff118e69677545fd890639f05389c9
whiskeeyy <kietduongdinh@gmail.com>
2025-10-08 19:55:06 +0700

    Remove Sidebar component from AdminLayout for cleaner structure

--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -1,10 +1,8 @@
-import Sidebar from "@/pages/admin/Sidebar";
 import { Outlet } from "react-router-dom";
 
 const AdminLayout = () => {
   return (
     <div style={{ backgroundColor: 'grey' }}>
-      <Sidebar />
       <div className="flex-1 p-4">
         <Outlet /> {/* MỌI TRANG CON hiển thị ở đây */}
       </div>
