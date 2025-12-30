bbc401e43269e0163f0312b82c8425c65924a4e8
whiskeeyy <kietduongdinh@gmail.com>
2025-12-30 23:41:38 +0700

    Update admin email and API base URL
    Changed the VITE_ADMIN_EMAIL in .env and updated the API_BASE_URL in axiosClient.ts from port 5273 to 5000 to reflect new configuration.

--- src/pages/axiosClient.ts
+++ src/pages/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-export const API_BASE_URL = "http://localhost:5273/api";
+export const API_BASE_URL = "http://localhost:5000/api";
 
 export const axiosClient = axios.create({
   baseURL: API_BASE_URL,
